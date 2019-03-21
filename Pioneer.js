6fd895cf6269b943469445c0a2ac8145a424dd48
Nick Soggin <nicksoggin@gmail.com>
2019-03-20 22:29:27 -0400

    mvp operational

--- Pioneer.js
+++ Pioneer.js
@@ -19,30 +19,166 @@ async function getHrefs( page ) {
 	} );
 }
 
-async function searchForLinks( page, ourl, cache ) {
-	const hrefs = await getHrefs( page );
+async function searchForLinks( browser, parent, cache ) {
+	const page = await browser.newPage();
+	await page.setRequestInterception( true );
 	
-	hrefs.forEach(
-		async ref => {
-			const host = new URL( ref );
+	page
+		.on( 'request', request => {
+			const url = new URL( request.url() );
 			
-			console.log( host );
+			// Set it up in case it doesn't respond
 			
-			ref = await page.goto( ref );
-			
-			// external page
-			if( host.hostname !== ourl.hostname ) {
-				cache.get( 'nodes' ).set( host.href, {
-					status: ref._status,
-					hostname: host,
-					external: true
-				} );
-			} else if( cache.has( host.href ) ) {
-				cache.get( 'nodes' ).get( host.href ).hasCircular = true;
+			if( cache.get( 'nodes' ).has( url.href ) ) {
+				return request.abort( 'aborted' );
 			}
 			
+			cache.get( 'nodes' ).set(
+				url.href,
+				{
+					url: url.href,
+					status: 0,
+					ok: 'unknown',
+					external: url.hostname !== parent.hostname,
+					parent: parent.href
+				}
+			);
+			
+			console.log( 'request  url:', url.href );
+			request.continue();
+		} )
+		.on( 'requestfailed', request => {
+			const url    = new URL( request.url() );
+			const reason = request.failure();
+			
+			if( reason.errorText === 'net::ERR_ABORTED' ) {
+				// Do nothing because we aborted it
+				return;
+			}
+			
+			// TODO:: check request response codes and cache
+			cache.get( 'nodes' ).set(
+				url.href,
+				{
+					url: url.href,
+					status: 0,
+					ok: reason.errorText,
+					external: url.hostname !== parent.hostname,
+					parent: parent.href
+				}
+			);
+			
+			console.log( 'request failed url:', url.href );
+		} )
+		.on( 'response', response => {
+			const request = response.request();
+			const status  = response.status();
+			const url     = new URL( request.url() );
+			const ref     = cache.get( 'nodes' ).get( url.href );
+			
+			ref.url           = url.href;
+			ref.status        = status;
+			ref.ok            = response.ok();
+			ref.external      = url.hostname !== parent.hostname;
+			ref.parent        = parent.href;
+			ref.remoteAddress = response.remoteAddress();
+			
+			const secDetails = response.securityDetails();
+			
+			if( secDetails ) {
+				ref.securityDetails = {
+					subjectName: secDetails.subjectName(),
+					issuer: secDetails.issuer(),
+					validFrom: secDetails.validFrom(),
+					validTo: secDetails.validTo(),
+					protocol: secDetails.protocol()
+				};
+			}
+			
+			console.log( 'response url:', url.href, 'status:', status );
+		} );
+	
+	await page.goto( parent, { waitUntil: 'networkidle2' } );
+	await page.waitFor( 1000 );
+	
+	let hrefs = await getHrefs( page );
+	
+	console.log( hrefs );
+	
+	for( let url of hrefs ) {
+		url = new URL( url );
+		
+		const href = url.href.replace( url.hash, '' );
+		
+		console.log( `eval href    : ${ href }` );
+		console.log( `cached       : ${ cache.get( 'nodes' ).has( href ) }` );
+		
+		if( cache.get( 'nodes' ).has( href ) ) {
+			const ref = cache.get( 'nodes' ).get( href );
+			
+			console.log( `found circular: ${ href }` );
+			// todo: link this up with some more info
+			ref.circular = ref.circular || [];
+			ref.circular.push( url.href );
+		} else if( url.hostname !== parent.hostname ) {
+			console.log( `eval ext href: ${ href }` );
+			
+			const extPage = await browser.newPage();
+			await extPage.setRequestInterception( false );
+			const extResponse = await extPage.goto( href );
+			
+			console.log( `ext href res : ${ extResponse.status() }` );
+			cache.get( 'nodes' ).set(
+				url.href,
+				{
+					url: url.href,
+					status: extResponse.status(),
+					ok: extResponse.ok(),
+					external: url.hostname !== parent.hostname,
+					parent: parent.href,
+					remoteAddress: extResponse.remoteAddress(),
+					securityDetails: {
+						subjectName: extResponse.securityDetails().subjectName(),
+						issuer: extResponse.securityDetails().issuer(),
+						validFrom: extResponse.securityDetails().validFrom(),
+						validTo: extResponse.securityDetails().validTo(),
+						protocol: extResponse.securityDetails().protocol()
+					}
+				}
+			);
+		} else {
+			console.log( `search url: ${ url.href }` );
+			await searchForLinks( browser, url, cache );
 		}
-	);
+	}
+	
+	// hrefs.forEach(
+	// 	resp => {
+	// 		console.log( resp );
+	//
+	// 		const refHost = new URL( resp._request._url );
+	//
+	// 		console.log( refHost );
+	//
+	// 		if( cache.has( refHost.href ) ) {
+	// 			return cache.get( 'nodes' ).get( refHost.href ).hasCircular = {
+	// 				// todo: link this up with some more info
+	// 				key: true
+	// 			};
+	// 		}
+	//
+	// 		// external page
+	// 		cache.get( 'nodes' ).set(
+	// 			refHost.href,
+	// 			{
+	// 				status: resp._status,
+	// 				hostname: refHost,
+	// 				external: refHost.hostname !== parent.hostname,
+	// 				parent: parent.href
+	// 			}
+	// 		);
+	// 	}
+	// );
 }
 
 ( async () => {
@@ -55,10 +191,10 @@ async function searchForLinks( page, ourl, cache ) {
 	// const x = await page.goto( 'https://docs.aws.amazon.com/' );
 	// await page.screenshot( { path: 'example.png' } );
 	
-	const cache = new LightMap();
-	const url   = 'https://parellin-technologies-llc.github.io/lightmap/';
-	const ourl  = new URL( url );
-	const x     = await page.goto( url );
+	const cache  = new LightMap();
+	const url    = 'https://parellin-technologies-llc.github.io/lightmap/';
+	const parent = new URL( url );
+	const x      = await page.goto( url );
 	
 	// console.log( x );
 	
@@ -76,11 +212,11 @@ async function searchForLinks( page, ourl, cache ) {
 	cache.set( 'dimensions', dimensions );
 	cache.set( 'security', [ x._securityDetails ] );
 	cache.set( 'nodes', new LightMap( [
-		[ url, {
-			status: x._status,
-			hostname: ourl,
-			base: true
-		} ]
+		// [ parent.href, {
+		// 	status: x._status,
+		// 	hostname: parent,
+		// 	parent: null
+		// } ]
 	] ) );
 	
 	// const walk = await page.evaluate( () => {
@@ -127,8 +263,7 @@ async function searchForLinks( page, ourl, cache ) {
 	// console.log( walk.pendingNavigation );
 	// console.log( walk.log );
 	
-	console.log( await searchForLinks( page, ourl, cache ) );
-	
+	await searchForLinks( browser, parent, cache );
 	
 	console.log( cache );
 	
