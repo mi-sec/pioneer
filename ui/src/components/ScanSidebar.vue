941d6c62520ab91e5240f51b5c0ee62c5ba1ea34
iskore <nicksoggin@gmail.com>
2019-06-18 12:29:48 -0400

    adding pdf report download
    # subsystem(s)
    # all lower case summary
    
    # description of primary changes
    # - added:
    # - updated:
    # - deleted:
    
    # Fixes: issue#xxx
    # Refs:
    #
    #

--- ui/src/components/ScanSidebar.vue
+++ ui/src/components/ScanSidebar.vue
@@ -15,7 +15,7 @@
 			<v-list-tile
 				v-for="item in items"
 				:key="item.title"
-				@click="scrollTo( item.title )"
+				@click="item.click ? item.click( item ) : scrollTo( item.title )"
 			>
 				<v-tooltip
 					nudge-right="15"
@@ -38,12 +38,22 @@
 </template>
 
 <script>
+	import html2canvas from 'html2canvas';
+	import JSPdf       from 'jspdf';
+	
+	global.html2canvas = html2canvas;
+	
 	export default {
 		name: 'ScanSidebar',
 		data() {
 			return {
 				model: null,
 				items: [
+					{
+						title: 'Download Report',
+						icon: 'mdi-file-download',
+						click: this.downloadReport
+					},
 					{ title: 'Summary', icon: 'dashboard' },
 					{ title: 'Performance', icon: 'mdi-speedometer' }
 				]
@@ -51,7 +61,23 @@
 		},
 		methods: {
 			scrollTo( id ) {
+				console.log( id );
 				id = id.toLowerCase();
+			},
+			async downloadReport() {
+				console.log( 'here' );
+				
+				const domElement = document.getElementById( 'summary' );
+				const pdf        = new JSPdf();
+				
+				pdf.html( domElement, {
+					html2canvas: html2canvas,
+					pageSize: 'a4',
+					callback: doc => {
+						console.log( 'x ' );
+						doc.save( 'your-filename.pdf' );
+					}
+				} );
 			}
 		}
 	};
